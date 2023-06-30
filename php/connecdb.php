00d236f74659a8bdcaf6c58923810866944b65f6
Guiza4 <youssef.guiza@yahoo.com>
2023-06-30 12:13:07 +0100

    First commit (php connecdb)

---
+++ php/connecdb.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+/*
+* Change the value of $password if you have set a password on the root userid
+* Change NULL to port number to use DBMS other than the default using port 3306
+*
+*/
+$user = 'root';
+$password = ''; //To be completed if you have set a password to root
+$database = 'yourmarket'; //To be completed to connect to a database. The database must exist.
+$port = 3306;
+$mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+echo '<p>Connection OK '. $mysqli->host_info.'</p>';
+echo '<p>Server '.$mysqli->server_info.'</p>';
+echo '<p>Initial charset: '.$mysqli->character_set_name().'</p>';
+$sql = "SELECT ID_Admin, First_Name FROM admin";
+$result = $mysqli->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["ID_Admin"]. " - Name: " . $row["First_Name"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$mysqli->close();
+?>
+</body></html>
