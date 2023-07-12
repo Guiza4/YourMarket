7ba190afd7a7b49c18017beb7750b5d60daed510
Guiza4 <youssef.guiza@yahoo.com>
2023-07-12 14:20:24 +0100

    On peut ajouter des produits

--- YourMarket/php/add-product.php
+++ YourMarket/php/add-product.php
@@ -1,5 +1,84 @@
 <?php
+$mysqli = require __DIR__ . "/connecdb.php";
+session_start();
+
+if (isset($_POST['add_product'])) {
+
+    $name = $_POST['name'];
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    $price = $_POST['price'];
+    $price = filter_var($price, FILTER_SANITIZE_STRING);
+    $category = $_POST['category'];
+    $category = filter_var($category, FILTER_SANITIZE_STRING);
+    $stock = $_POST['stock'];
+    $stock = filter_var($stock, FILTER_SANITIZE_STRING);
+    $details = $_POST['details'];
+    $details = filter_var($details, FILTER_SANITIZE_STRING);
+
+    $image_1 = $_FILES['image_1']['name'];
+    $image_1 = filter_var($image_1, FILTER_SANITIZE_STRING);
+    $image_size_1 = $_FILES['image_1']['size'];
+    $image_tmp_name_1 = $_FILES['image_1']['tmp_name'];
+    $image_folder_1 = '../uploaded_img/' . $image_1;
+
+    $image_2 = $_FILES['image_2']['name'];
+    $image_2 = filter_var($image_2, FILTER_SANITIZE_STRING);
+    $image_size_2 = $_FILES['image_2']['size'];
+    $image_tmp_name_2 = $_FILES['image_2']['tmp_name'];
+    $image_folder_2 = '../uploaded_img/' . $image_2;
+
+    $image_3 = $_FILES['image_3']['name'];
+    $image_3 = filter_var($image_3, FILTER_SANITIZE_STRING);
+    $image_size_3 = $_FILES['image_3']['size'];
+    $image_tmp_name_3 = $_FILES['image_3']['tmp_name'];
+    $image_folder_3 = '../uploaded_img/' . $image_3;
+
+    $select_products = $mysqli->prepare("SELECT * FROM `article` WHERE name = ?");
+    $select_products->bind_param("s", $name); // Bind the parameter
+    $select_products->execute(); // Execute the statement
+    $result = $select_products->get_result(); // Get the result set
+    $rowCount = $result->num_rows; // Get the row count
+
+    if ($rowCount > 0) {
+        $message[] = 'product name already exists!';
+    } else {
+        $insert_products = $mysqli->prepare("INSERT INTO `article`(name, details, price, category, stock, image_1, image_2, image_3) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        $insert_products->bind_param("ssssssss", $name, $details, $price, $category, $stock, $image_1, $image_2, $image_3);
+        $insert_products->execute();
+
+        if ($insert_products) {
+            if ($image_size_1 > 2000000 or $image_size_2 > 2000000 or $image_size_3 > 2000000) {
+                $message[] = 'image size is too large!';
+            } else {
+                move_uploaded_file($image_tmp_name_1, $image_folder_1);
+                move_uploaded_file($image_tmp_name_2, $image_folder_2);
+                move_uploaded_file($image_tmp_name_3, $image_folder_3);
+                $message[] = 'new product added!';
+            }
+        }
+    }
+}
+
+if (isset($_GET['delete'])) {
+    $delete_id = $_GET['delete'];
+    $delete_product_image = $mysqli->prepare("SELECT * FROM `article` WHERE id = ?");
+    $delete_product_image->bind_param("i", $delete_id);
+    $delete_product_image->execute();
+    $fetch_delete_image = $delete_product_image->fetch(PDO::FETCH_ASSOC);
+    unlink('../uploaded_img/' . $fetch_delete_image['image_1']);
+    unlink('../uploaded_img/' . $fetch_delete_image['image_2']);
+    unlink('../uploaded_img/' . $fetch_delete_image['image_3']);
+    $delete_product = $mysqli->prepare("DELETE FROM `article` WHERE id = ?");
+    $delete_product->bind_param("i", $delete_id);
+    $delete_product->execute();
+    $delete_cart = $mysqli->prepare("DELETE FROM `cart` WHERE pid = ?");
+    $delete_cart->bind_param("i", $delete_id);
+    $delete_cart->execute();
+    header('location:add-product.php');
+}
+
 ?>
+
 <!DOCTYPE html>
 <head>
     <title>Add product</title>
@@ -26,10 +105,10 @@
             <span>Account</span>
         </div>
     </a>
-    <a href="#">
-        <div class="nav-cart">
-            <img src="../image/cart.png" width="38" height="34">
-            <span>Cart</span>
+    <a href="add-product.php">
+        <div class="nav-sellings">
+            <img src="../image/sellings.png" width="38" height="34">
+            <span>Sellings</span>
         </div>
     </a>
 </div>
@@ -43,7 +122,7 @@
         <div class="bar-random">
             <!--cette bare ne sert completment a rien mais ca fait class et c marant a faire-->
         </div>
-        <form action="" method="post" enctype="multipart/form-data">
+        <form method="post" enctype="multipart/form-data">
             <div class="flex">
                 <div class="inputBox">
                     <span>Product name (required)</span>
@@ -57,7 +136,7 @@
                 </div>
                 <div class="inputBox">
                     <span>Category (required)</span>
-                    <select class="box">
+                    <select class="box" name="category">
                         <option>Phone</option>
                         <option>Computer</option>
                         <option>Watch</option>
@@ -66,22 +145,22 @@
                 </div>
                 <div class="inputBox">
                     <span>Stock (required)</span>
-                    <input type="number"  min="1" class="box" required max="9999999999" placeholder="Enter Quantity"
+                    <input type="number" min="1" class="box" required max="9999999999" placeholder="Enter Quantity"
                            name="stock">
                 </div>
                 <div class="inputBox">
-                    <span>image 01 (required)</span>
-                    <input type="file" name="image_01" accept="image/jpg, image/jpeg, image/png, image/webp" class="box"
+                    <span>image 1 (required)</span>
+                    <input type="file" name="image_1" accept="image/jpg, image/jpeg, image/png, image/webp" class="box"
                            required>
                 </div>
                 <div class="inputBox">
-                    <span>image 02 (required)</span>
-                    <input type="file" name="image_02" accept="image/jpg, image/jpeg, image/png, image/webp" class="box"
+                    <span>image 2 (required)</span>
+                    <input type="file" name="image_2" accept="image/jpg, image/jpeg, image/png, image/webp" class="box"
                            required>
                 </div>
                 <div class="inputBox">
-                    <span>image 03 (required)</span>
-                    <input type="file" name="image_03" accept="image/jpg, image/jpeg, image/png, image/webp" class="box"
+                    <span>image 3 (required)</span>
+                    <input type="file" name="image_3" accept="image/jpg, image/jpeg, image/png, image/webp" class="box"
                            required>
                 </div>
                 <div class="inputBox">
