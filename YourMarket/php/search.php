000bedb5ca7e47006e803ca1ba3eb6515db27210
Guiza4 <youssef.guiza@yahoo.com>
2023-07-13 17:23:47 +0100

    search.php included in index.php

--- YourMarket/php/search.php
+++ YourMarket/php/search.php
@@ -1,37 +1,16 @@
 <?php
 $mysqli = require __DIR__ . "/connecdb.php";
-$allproducts = $mysqli->query('SELECT * FROM article ORDER BY ID_Article DESC');
-if (isset($GET['search']) and !empty($_GET['search'])) {
-    $searchbar = htmlspecialchars($GET['search']);
-    $allproducts = $mysqli->query('SELECT name FROM article WHERE name LIKE "%' . $searchbar . '%" ORDER BY ID_Article DESC');
+$products = [];
+
+if (isset($_GET['search']) && !empty($_GET['search'])) {
+    $searchbar = htmlspecialchars($_GET['search']);
+    $searchQuery = 'SELECT name FROM article WHERE name LIKE "%' . $searchbar . '%" ORDER BY ID_Article DESC';
+    $result = $mysqli->query($searchQuery);
+
+    if ($result && $result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row['name'];
+        }
+    }
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Search Products</title>
-    <meta charset="utf-8">
-</head>
-<body>
-<form method="get">
-    <input type="search" name="search" placeholder="Search Product" autocomplete="off">
-    <input type="submit" name="send">
-</form>
-
-<section class="show_products">
-    <?php
-    if ($allproducts->rowCount() > 0) {
-        while ($product = $allproducts->fetch()) {
-            ?>
-            <p><?= $product['name']; ?></p>
-            <?php
-        }
-    } else {
-        ?>
-        <p>No Products Found !</p>
-        <?php
-    }
-    ?>
-</section>
-</body>
-</html>
